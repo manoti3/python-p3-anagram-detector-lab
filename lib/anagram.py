00a0d9c295527ccf2521bab69a54822d4c682065
manoti3 <hillarymanoti0@gmail.com>
2024-03-05 23:29:13 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word, anagramiclist=list()):
+        self.word = word
+        self.anagramiclist = anagramiclist
+    
+    def match(self, list):
+        self.anagramiclist = []
+        wordcheck = sorted([letter for letter in self.word])
+        print(f'original word:{self.word}')
+        for entry in list:
+            entrycheck = sorted([letter for letter in entry])
+            if wordcheck == entrycheck:
+                self.anagramiclist.append(entry)
+        print(self.anagramiclist)
+        return self.anagramiclist
